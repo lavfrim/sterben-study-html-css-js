b5c477174ce75ee2375152318dab1280d58ee0dd
lavfrim <47713188+lavfrim@users.noreply.github.com>
2024-02-17 17:51:22 +0400

    create random color button (#1)

--- main.js
+++ main.js
@@ -1,1 +1,28 @@
  console.log('--- run main.js ----');
+
+ const contentContainer = document.querySelector('#content');
+ const title = document.querySelector('#title');
+
+ const createButton = (name, className, onClickHandler) => {
+    const buttonElement = document.createElement('button');
+
+    buttonElement.textContent = name;
+    buttonElement.className = className;
+    buttonElement.addEventListener('click', onClickHandler);
+
+    return buttonElement;
+ }
+
+ const createRandomNumberFrom0To255 = () => {
+    return Math.floor(Math.random() * 255);
+ };
+ const getRandomColor = () => {
+    return `rgb(${createRandomNumberFrom0To255()}, ${createRandomNumberFrom0To255()}, ${createRandomNumberFrom0To255()})`
+ }
+ const randomColarButtonOnClickHandler = () => {
+    title.style.color = getRandomColor();
+ }
+
+const randomColarButton = createButton('Change Color', 'button-style',  randomColarButtonOnClickHandler);
+
+contentContainer.appendChild(randomColarButton);
